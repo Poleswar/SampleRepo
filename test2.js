70e77afe6ffe3358d5bdf3369384201d91dcbf4f
Eswar <eswar.p@cloudodyssey.co.uk>
2023-12-06 18:07:23 +0530

    test2 Commit

---
+++ test2.js
@@ -0,0 +1,21 @@
+export default class ApexImperativeMethodWithParams extends LightningElement {
+    searchKey = "";
+    contacts;
+    error;
+  
+    handleKeyChange(event) {
+      this.searchKey = event.target.value;
+    }
+  
+    handleSearch() {
+      findContacts({ searchKey: this.searchKey })
+        .then((result) => {
+          this.contacts = result;
+          this.error = undefined;
+        })
+        .catch((error) => {
+          this.error = error;
+          this.contacts = undefined;
+        });
+    }
+  }
